5adc8973531ab5fcaa3f60b36af3aa7c561cb4a3
javakc <javakc@javakc.com>
2020-08-07 16:21:16 +0800

    章节测试实体类

--- service/service-cms/src/main/java/com/javakc/cms/entity/ChapterTest.java
+++ service/service-cms/src/main/java/com/javakc/cms/entity/ChapterTest.java
@@ -1,22 +1,50 @@
 package com.javakc.cms.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.util.Date;
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name ="import_chapter")
 public class ChapterTest {
     /** 主键 */
     @Id
-    @GeneratedValue
+    @Column(name = "id")
+    @ApiModelProperty(value = "章节测试主键")
     private Integer id ;
     /** 章节名称 */
+    @Column(name = "chapter_name")
+    @ApiModelProperty(value = "章节名称")
     private String chapterName ;
     /** 章节序号 */
+    @Column(name = "chapter_sort")
+    @ApiModelProperty(value = "章节序号")
     private Integer chapterSort ;
     /** 章节状态 */
+    @Column(name = "chapter_status")
+    @ApiModelProperty(value = "章节状态")
     private Integer chapterStatus ;
-    /** 更新时间 */
-    private Date updateTime ;
     /** 外键 */
     private Book book;
+
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss", timezone = "GMT+8")
+    @ApiModelProperty(value = "创建时间",example = "2020-12-12 12:12:12")
+    @Column(name = "gmt_create",nullable = false, updatable = false)
+    private Date gmtCreate;
+
+
+    @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss", timezone = "GMT+8")
+    @ApiModelProperty(value = "修改时间",example = "2020-12-12 12:12:12")
+    @Column(name = "gmt_modified",nullable = false, insertable = false)
+    private Date gmtModified;
+
 }
